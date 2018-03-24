1ebf5e9993e9e61c84b283da57be49e59ad1fb61
James Pinnock <james@Jamess-MacBook-Pro.local>
2018-03-24 08:59:54 +0000

    White spaces cleaned up and formating sorted with naming conventions

--- Search/src/search/Search.java
+++ Search/src/search/Search.java
@@ -6,15 +6,7 @@ package search;
  */
 public class Search {
 
-    /**
-     * Returns the index of the longest string in the list. If there are several
-     * string of this length, the indexed returned is the that of the first.
-     *
-     * @param a list of strings, in ascending order
-     * @return position of an entry with the longest string in the list
-     */ //W
     public int longestWord(StringList a) {
-        // replace the following line with your implementation
 
 //        int largetWord = a.get(0).length();
         int index = 0;
@@ -28,7 +20,7 @@ public class Search {
                 i = a.get(i).length();
 
             }
-            
+
             i = i + 1;
 
         }
@@ -44,78 +36,51 @@ public class Search {
      */
     public int countUnique(StringList a) {
 
-//         replace the following line with your implementation
-
         int i = 1;
         int c = 0;
         int j = 0;
 
         boolean unique = true;
 
-        while (i < a.size()) {            
-                        
-            if (a.get(i) != a.get(i-1)) {
+        while (i < a.size()) {
 
-                c = c +1;
-            }else{
-//                a.get(i) != a.get(j);
-                c = c +1;
-                
+            if (a.get(i) != a.get(i - 1)) {
+                c = c + 1;
+            } else {
+                c = c + 1;
             }
-            
             i = i + 1;
         }
         return c;
-
     }
 
-    /**
-     * Returns the most commonly occurring string in the list. If two or more
-     * are equally common, return the one that comes earliest.
-     *
-     * @param a collection of strings, in ascending order
-     * @return most frequently occurring string
-     */
     public String mostFrequent(StringList a) {
-        // replace the following line with your implementation
-        
+
         String previous = null;
-        String repeated = null;
-        
+        String Frequent = null;
+
         int reptd = 0;
         int i = 0;
         int c = 0;
-        
-        while (i < a.size()){
-            if (!a.get(i).equals(previous)){
+
+        while (i < a.size()) {
+            if (!a.get(i).equals(previous)) {
                 c = 0;
                 previous = a.get(i);
-            }else if(c > reptd){
+            } else if (c > reptd) {
                 reptd = c;
-                repeated= previous;
-            }else{
+                Frequent = previous;
+            } else {
                 c = c + 1;
             }
-            
             i = i + 1;
         }
-        
-        System.out.println("The most Freuqent word: " + repeated);
-
-        return repeated;
+        return Frequent;
     }
 
-
-    /**
-     * Search for a string in an ordered collection
-     *
-     * @param a collection of strings, in ascending order
-     * @param k string to search for
-     * @return position of an element equal to k, if any, otherwise -1
-//     */ //W
     public int findElement(StringList a, String k) {
         // replace the following line with your implementation
-        
+
         int lo = 0;
         int hi = a.size() - 1;
         int mid;
@@ -133,10 +98,7 @@ public class Search {
         }
 
         return -1;
-    } 
-
-
-    
+    }
 
     /**
      * Position of a string in an ordered collection
@@ -150,41 +112,23 @@ public class Search {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    /**
-     * Position after a string in an ordered collection
-     *
-     * @param a collection of strings, in ascending order
-     * @param k string to search for
-     * @return number of strings in the collection strictly greater than k
-     */ //W
     public int countGreater(StringList a, String k) {
         // replace the following line with your implementation
-        
-        int hi = a.size()-1;
-        
-        int mid =0;
-        
-        int lo =0;
-        
-        
-        
-        while(lo <= hi){
-            
+
+        int hi = a.size() - 1;
+        int mid = 0;
+        int lo = 0;
+
+        while (lo <= hi) {
             mid = (hi + lo) / 2;
-            
-            if(a.get(mid).compareTo(k) <= 0)
-            {
-             lo = mid + 1;
-                
-            }else{
-                
+            if (a.get(mid).compareTo(k) <= 0) {
+                lo = mid + 1;
+            } else {
                 hi = mid - 1;
             }
         }
-            return(a.size()-lo);
+        return (a.size() - lo);
     }
-
-    
 
     /**
      * Determine the size of a range in an ordered collection
@@ -199,14 +143,6 @@ public class Search {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    /**
-     * Count partial matches in an ordered collection
-     *
-     * @param a collection of strings, in ascending order
-     * @param prefix first string to search for
-     * @param substring second string to search for
-     * @return number of strings with both the prefix and the substring
-     */
     public int countMatches(StringList a, String prefix, String substring) {
         // replace the following line with your implementation
         String at;
@@ -220,5 +156,4 @@ public class Search {
         }
         return count;
     }
-
 }
